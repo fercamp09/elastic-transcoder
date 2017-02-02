27f4578eb2cc285b087d6c27e5cd1bfa2347ec49
fercamp09 <fercamp09@gmail.com>
2017-02-02 00:46:40 +0000

    Changes for implementing ReadTask and CancelTask

--- rpc_client.go
+++ rpc_client.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"time"
 	"strconv"
+	"strings"
 
 	"github.com/streadway/amqp"
 	"github.com/golang/protobuf/proto"
@@ -31,11 +32,19 @@ func randInt(min int, max int) int {
 	return min + rand.Intn(max-min)
 }
 
-func imageRPC(i string, o string, p int) (resp *pb.Response, err error) {
+func connectToRabbitMQ() (*amqp.Connection, error) {
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
+	return conn, err
+}
 
+func imageRPC(i string, o string, p int) (resp *pb.Response, err error) {
+	//conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	//failOnError(err, "Failed to connect to RabbitMQ")
+	//defer conn.Close()
+	conn, err := connectToRabbitMQ()
+	
 	ch, err := conn.Channel()
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
@@ -64,30 +73,32 @@ func imageRPC(i string, o string, p int) (resp *pb.Response, err error) {
 	)
 	failOnError(err, "Failed to register a consumer")
 	corrId := randomString(32)
-
+	log.Printf("ID: ", corrId)
+ 
         // Encode with protocol buffer
-	t := &pb.Task{
-        	Filename:  i,
-        	NewName:  o,
-        	Priority:  int32(p),
-	}
-	out, err := proto.Marshal(t)
-       	failOnError(err, "Failed to encode task:")
-
+	//t := &pb.Task{
+        //	Filename:  i,
+       // 	NewName:  o,
+       // 	Priority:  int32(p),
+	//	FileId: corrId,
+	//}
+	//out, err := proto.Marshal(t)
+       	//failOnError(err, "Failed to encode task:")
+	
 	// Publish task
-	err = ch.Publish(
-		"",          // exchange
-		"rpc_queue2",// routing key
-		false,       // mandatory
-		false,       // immediate
-		amqp.Publishing{
-			ContentType:   "application/octet-stream",
-			CorrelationId: corrId,
-			ReplyTo:       q.Name,
-			Priority:      uint8(p),
-			Body:          out,
-		})
-	failOnError(err, "Failed to publish a message")
+	//err = ch.Publish(
+	//	"",          // exchange
+	//	"rpc_queue2",// routing key
+	//	false,       // mandatory
+	//	false,       // immediate
+	//	amqp.Publishing{
+	//		ContentType:   "application/octet-stream",
+	//		CorrelationId: corrId,
+	//		ReplyTo:       q.Name,
+	//		Priority:      uint8(p),
+	//		Body:          out,
+	//	})
+	//failOnError(err, "Failed to publish a message")
 
 	for d := range msgs {
 		if corrId == d.CorrelationId {
@@ -102,15 +113,116 @@ func imageRPC(i string, o string, p int) (resp *pb.Response, err error) {
 	return
 }
 
+func cancelTask(id string){
+	conn, err := connectToRabbitMQ()
+
+        ch, err := conn.Channel()
+        failOnError(err, "Failed to open a channel")
+        defer ch.Close()
+
+        args := make(amqp.Table)
+        args["x-max-priority"] = int32(2)
+
+        q, err := ch.QueueDeclare(
+                "cancel_queue",    // name
+                false, // durable
+                false, // delete when usused
+                true,  // exclusive
+                false, // noWait
+                args,   // arguments
+        )
+	failOnError(err, "Failed to declare a queue")
+
+        //corrId := randomString(32)
+	corrId := id
+        
+	// Encode with protocol buffer
+        t := &pb.Cancel{
+		FileId:  corrId,
+        }
+        out, err := proto.Marshal(t)
+        failOnError(err, "Failed to encode task:")
+
+        // Publish task
+        err = ch.Publish(
+                "",          // exchange
+                q.Name,      // routing key
+                false,       // mandatory
+                false,       // immediate
+                amqp.Publishing{
+                        ContentType:   "application/octet-stream",
+                        Body:          out,
+                })
+        failOnError(err, "Failed to publish a message")
+
+        log.Printf("Task %s cancelled", corrId)
+
+        return
+}
+
+func readTask(id string){
+	conn, err := connectToRabbitMQ()
+	
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	args := make(amqp.Table)
+        args["x-max-priority"] = int32(2)
+	
+	q, err := ch.QueueDeclare(
+		"rpc_queue2", // name
+		false,   // durable
+		false,   // delete when usused
+		false,   // exclusive
+		false,   // no-wait
+		args,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		false,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	for d := range msgs {
+		task := &pb.Task{}
+                err := proto.Unmarshal(d.Body, task)
+                failOnError(err, "Failed to parse task message")
+                if id == task.FileId {
+                      log.Printf("FileId: %s, Filename:%s, New Filename: %s, New Format: %s", task.FileId, task.Filename, task.NewName, task.Priority, task.Format)
+                      break
+		}
+	}
+
+	log.Printf(" [*] Searching for message. To exit press CTRL+C")
+	
+}
+
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
-
-	input, output, filetype, priority := bodyFrom(os.Args)
-	log.Printf(" [x] Requesting image(%s)", input)
-	res, err := imageRPC(input, output, priority)
-	failOnError(err, "Failed to handle RPC request")
-
-	log.Printf(" [.] Image processed found in %s, %s", res.FileLocation, filetype)
+	if strings.Compare(os.Args[1],"create") == 0 {
+		input, output, filetype, priority := bodyFrom(os.Args)
+		log.Printf(" [x] Requesting image(%s)", input)
+		res, err := imageRPC(input, output, priority)
+		failOnError(err, "Failed to handle RPC request")
+		log.Printf(" [.] Image processed found in %s, %s", res.FileLocation, filetype)
+	} else if strings.Compare(os.Args[1], "read") == 0 {
+		id := os.Args[2]
+		readTask(id)
+	} else if os.Args[1] == "cancel" {
+		id := os.Args[2]
+		cancelTask(id)
+	} else {
+		log.Printf("Wrong arguments, valid options: read, cancel, create")
+	}
+	
 }
 
 func bodyFrom(args []string) (string, string, string, int) {
@@ -118,22 +230,21 @@ func bodyFrom(args []string) (string, string, string, int) {
 	var p int
 	var err error
 
-	log.Print(len(os.Args))
-	if len(os.Args) < 4 {
+	if len(os.Args) < 5 {
 		log.Print("Please supply an input and output filename e.g. go run rpc_client.go input.jpg output.jpg jpg 1")
 		os.Exit(3)
-		} else if len(os.Args) == 4 {
+		} else if len(os.Args) == 5 {
  		p = 0
-		i = os.Args[1]
-		o = os.Args[2]
-		s = os.Args[3]
+		i = os.Args[2]
+		o = os.Args[3]
+		s = os.Args[4]
 		log.Print("Added to Non-Priority Queue")
 	} else {
-		i = os.Args[1]
-		o = os.Args[2]
-		s = os.Args[3]
-		p, err = strconv.Atoi(os.Args[4])
+		i = os.Args[2]
+		o = os.Args[3]
+		s = os.Args[4]
+		p, err = strconv.Atoi(os.Args[5])
 	}
-	failOnError(err, "wrong arguments")
+	failOnError(err, "Wrong arguments")
 	return i, o, s, p
 }
